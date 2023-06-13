09f9dcc7f292551d7850a574d92935ed79b207e6
ndng1410 <ndng1410@gmail.com>
2023-06-13 09:06:30 +0700

    update UI + encrypt RSA/AES testing

--- resources/js/components/List.vue
+++ resources/js/components/List.vue
@@ -149,7 +149,7 @@ export default {
             // Chuyển đổi sang định dạng AM/PM
             let hours = date.getHours() % 12 || 12;
             let minutes = date.getMinutes();
-            let period = date.getHours() >= 12 ? "pm" : "am";
+            let period = date.getHours() >= 12 ? " PM" : " AM";
             // Chuyển đổi sang định dạng ngày/tháng/năm
             let day = date.getDate();
             let month = date.getMonth() + 1; // Tháng trong JavaScript đếm từ 0, nên cần +1
